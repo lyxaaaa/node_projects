feb41a1d171b1579187033cfefad45da0a58f34a
lyxaaaa <2780197685@qq.com>
2023-12-27 12:26:35 +0800

    feat:basic static page

--- project_accounts/routes/index.js
+++ project_accounts/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//记账本列表
+router.get('/account', function(req, res, next) {
+  res.render('list');
 });
+
+//添加记录
+router.get('/account/create', (req, res, next) => {
+  res.render('create');
+})
+
+router.post('/account/create', (req, res, next) => {
+  res.send('successful');
+})
 
 module.exports = router;
